64841e56c501ba36d95b39fae1fe1474da77375c
seladb <pcapplusplus@gmail.com>
2021-06-01 18:13:20 -0700

    Few fixes to queue implementation

--- common/queue.py
+++ common/queue.py
@@ -1,8 +1,17 @@
+from dataclasses import dataclass
 from typing import Optional
 
 
+@dataclass()
 class Job:
-    pass
+    id: int
+
+
+class Node:
+    def __init__(self, job=None):
+        self.item = job
+        self.next = None
+        self.previous = None
 
 
 class JobQueue:
@@ -12,7 +21,7 @@ class JobQueue:
         self.tail = None
 
     def enqueue(self, job: Job):
-        new_node = job
+        new_node = Node(job)
         if self.head is None:
             self.head = self.tail = new_node
         else:
@@ -21,9 +30,11 @@ class JobQueue:
             self.tail = new_node
 
     def dequeue(self) -> Optional[Job]:
-        job = self.head
+        if not self.head:
+            return None
+        node = self.head
         self.head = self.head.next
         self.length -= 1
         if self.length == 0:
             self.tail = None
-        return job
+        return node.item

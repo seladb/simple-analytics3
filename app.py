c287e2808343aef4c33b0234a2009a81836cdfdb
seladb <pcapplusplus@gmail.com>
2021-06-01 17:55:32 -0700

    - Add queue implementation - Implement multi-threaded queue

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 # https://docs.python.org/3/library/queue.html
 import threading
 import queue
+import uuid
 from flask import Flask, request
 from flask.logging import default_handler
 from services.enrich_geo_location import EnrichGeoLocation
@@ -15,21 +16,36 @@ root = logging.getLogger()
 root.setLevel(logging.INFO)
 root.addHandler(default_handler)
 
+job_queue = queue.Queue()
+
+
+def worker():
+    while True:
+        job_id, ip_addr, headers = job_queue.get()
+        logging.info(f"Working on {job_id}")
+        enrich_geo = EnrichGeoLocation()
+        enriched_data = enrich_geo.enrich(ip_addr)
+
+        anomaly_rank = AnomalyRanking()
+        request_rank = anomaly_rank.rank_request(headers)
+
+        db_service = DBService()
+        db_service.write_to_db(
+            Record(
+                geo_location=enriched_data, rank=request_rank
+            )
+        )
+        logging.info(f"Finished {job_id}")
+        job_queue.task_done()
+
+
+# turn-on the worker thread
+threading.Thread(target=worker, daemon=True).start()
+
 
 @app.route('/')
 def serve_requests():
-    enrich_geo = EnrichGeoLocation()
-    enriched_data = enrich_geo.enrich(request.remote_addr)
-
-    anomaly_rank = AnomalyRanking()
-    request_rank = anomaly_rank.rank_request(request.headers)
-
-    db_service = DBService()
-    db_service.write_to_db(
-        Record(
-            geo_location=enriched_data, rank=request_rank
-        )
-    )
+    job_queue.put((uuid.uuid4(), request.remote_addr, request.headers))
     return "Hello world"
 
 
--- common/queue.py
+++ common/queue.py
@@ -7,10 +7,23 @@ class Job:
 
 class JobQueue:
     def __init__(self):
-        pass
+        self.length = 0
+        self.head = None
+        self.tail = None
 
     def enqueue(self, job: Job):
-        pass
+        new_node = job
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
 
     def dequeue(self) -> Optional[Job]:
-        return None
+        job = self.head
+        self.head = self.head.next
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return job
